0c66e8350c2a8ce5634829b2d7716b2e7ba647cf
UserAdmin <UserAdmin@DESKTOP-A9EKHKQ>
2022-05-09 08:12:05 +0200

    Add project files.

---
+++ Clientside/Pages/Aboutme.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Clientside.Pages
+{
+    public class AboutmeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
